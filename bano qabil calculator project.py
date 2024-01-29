b3c61c084d31e2483cf8d3dff228557ceed4f588
VISUALARMOR03 <157637948+VISUALARMOR03@users.noreply.github.com>
2024-01-29 23:05:24 +0500

    Add files via upload

---
+++ bano qabil calculator project.py
@@ -0,0 +1,19 @@
+print("~~~~~~~BANO QABIL CALCULATOR~~~~~~~")
+
+num1=float(input("inter first numberr here:"))
+num2=float(input("inter second numberr here:"))
+
+print("press 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 for division")
+
+choice=int(input("inter your choice 1-4:"))
+
+if choice==1:
+   print(num1+num2)
+elif choice==2:
+   print(num1-num2)
+elif choice == 3:
+   print(num1*num2)
+elif choice==4:
+    print(num1/num2)
+else:
+    print("invalit input")
